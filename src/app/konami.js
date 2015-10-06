f978ddaad9e61eb851863faf60ba9dca4b90330b
pcaron11 <pcaron@att.net>
2015-10-06 16:38:14 -0400

    Refine styles and add media queries
    Add konami code functionality, just watch out for Metal Gear

---
+++ src/app/konami.js
@@ -0,0 +1,11 @@
+(function($) {
+	//Don't worry, this easter egg is NOT in another castle
+    'use strict';
+
+    var egg = new Egg("up,up,down,down,left,right,left,right,b,a", function() {
+        $('#konami').show()
+        window.setTimeout(function() {
+        	$('#konami').css('display', 'none'); 
+        }, 5000);
+    }).listen();
+})(jQuery);
